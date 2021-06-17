79f4e5b3b27a976ed0546e6de74329c1c8c73528
spyrstab <stampol23@hotmail.gr>
2021-06-17 14:26:51 +0300

    Updated

--- tennis/sw.js
+++ tennis/sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", e=> {
     e.waitUntil(
         chaces.open("static").then(cache=>
             {
-                return cache.addAll([,"./AΓΩΝΕΣ.html","./ΑΡΧΙΚΗ.html","./ΒΑΘΜΟΛΟΓΙΑ.html","./ΕΠΙΚΟΙΝΩΝΙΑ.html",
+                return cache.addAll(["./index.html","./AΓΩΝΕΣ.html","./ΑΡΧΙΚΗ.html","./ΒΑΘΜΟΛΟΓΙΑ.html","./ΕΠΙΚΟΙΝΩΝΙΑ.html",
                 ,"/Tennis.png","/tsitsipas.jpg","./style.css","./icon.png"])
             })
     );
